0c1cbcc922821b812e8324ef2cb599d8b6c237d1
RedDante <92797345+RedDante@users.noreply.github.com>
2023-11-26 22:01:05 +0000

    dz

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,18 @@
-function makeTransaction(quantity, pricePerDroid) {
-    const totalCost = quantity * pricePerDroid;
-    const purchaseMessage = `You ordered ${quantity} droids worth ${totalCost} credits!`;
-    return purchaseMessage;
-}
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
+//     let message;
+//     let totalPrice = quantity * pricePerDroid;
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+//     if (totalPrice > customerCredits) {
+//         message = 'Insufficient funds!';
+//     } else {
+//         message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+//     }
+
+//     return message;
+// }
+
+// console.log(makeTransaction(5, 3000, 23000));
+// console.log(makeTransaction(3, 1000, 15000));
+// console.log(makeTransaction(10, 5000, 8000));
+// console.log(makeTransaction(8, 2000, 10000));
+// console.log(makeTransaction(10, 500, 5000));
--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,18 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+function checkForSpam(message) {
+    const spanWord1 = 'spam';
+    const spanWord2 = 'sale';
 
-    return totalWidth;
+    const hasSpan1 = message.toLowerCase().includes(spanWord1);
+    const hasSpan2 = message.toLowerCase().includes(spanWord2);
+    const results = hasSpan1 || hasSpan2;
+
+    return results;
 }
 
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
-// sdgdfgdf
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
